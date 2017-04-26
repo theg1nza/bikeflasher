70903a970f0c3071a6e06ca9a8e08376ae601bb6
Jean-Simon Desjardins <jsdesjardinssds@gmail.com>
2017-04-26 10:12:03 -0400

    Update bike_flasher.ino

--- bike_flasher.ino
+++ bike_flasher.ino
@@ -4,31 +4,34 @@
 #include "LedControl.h"
 #include "binary.h"
 
-/*const DIN = 12;
-  const CS = 11;
-  const CLK = 10; */
+/*const int DIN = 12;
+  const int CS = 11;
+  const int CLK = 10; */
+
+const int rightButton = 9;
+const int leftButton = 8;
 
 LedControl lc = LedControl (12,11,10,1);
 
 //Delay time between updates (mandatory)
 unsigned long delaytime = 100;
 
+int rightButtonState = 0;
+int leftButtonState = 0;
+
 void setup() {
   lc.shutdown(0, false);
   lc.setIntensity(0, 8);
   lc.clearDisplay(0);
-}
 
-void writeArduinoOnMatrix() {
-  
-  byte left[8]  = {0x0, 0x38, 0x70, 0xff, 0xff, 0x70, 0x38, 0x0};
-  byte off[8]   = {0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0};
-  byte brake[8] = {0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff};
-  
+  pinMode(rightButton, INPUT);
+  pinMode(leftButton, INPUT);
 }
 
 void rightArrow() {
-  byte right[8] = {0x0, 0x1c, 0xe, 0xff, 0xff, 0xe, 0x1c, 0x0};
+  byte right[8]  = {0x38,0x1c,0xe,0xff,0xff,0xe,0x1c,0x38};
+
+  lc.setIntensity(0, 10);
   
   lc.setRow(0,0,right[0]);
   lc.setRow(0,1,right[1]);
@@ -43,8 +46,47 @@ void rightArrow() {
   delay(600);
 }
 
+void leftArrow() {
+  byte left[8]  = {0x1c,0x38,0x70,0xff,0xff,0x70,0x38,0x1c};
+
+  lc.setIntensity(0, 10);
+  
+  lc.setRow(0,0,left[0]);
+  lc.setRow(0,1,left[1]);
+  lc.setRow(0,2,left[2]);
+  lc.setRow(0,3,left[3]);
+  lc.setRow(0,4,left[4]);
+  lc.setRow(0,5,left[5]);
+  lc.setRow(0,6,left[6]);
+  lc.setRow(0,7,left[7]);
+  delay(600);
+  lc.clearDisplay(0);
+  delay(600);
+}
+
+void brake() {  
+  byte brake[8] = {0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff};
+  
+  lc.setIntensity(0, 1);
+
+  lc.setRow(0,0,brake[0]);
+  lc.setRow(0,1,brake[1]);
+  lc.setRow(0,2,brake[2]);
+  lc.setRow(0,3,brake[3]);
+  lc.setRow(0,4,brake[4]);
+  lc.setRow(0,5,brake[5]);
+  lc.setRow(0,6,brake[6]);
+  lc.setRow(0,7,brake[7]);
+}
+
 void loop() {
-  //writeArduinoOnMatrix();
-  //lc.setLed(0,0,1,true);
-  rightArrow();
+  rightButtonState = digitalRead(rightButton);
+  leftButtonState = digitalRead(leftButton);
+
+  if (rightButtonState == HIGH) {
+    brake();
+  }
+  
+  //rightArrow();
+  //leftArrow();
 }
