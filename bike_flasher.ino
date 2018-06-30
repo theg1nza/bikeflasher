fc0518eda0b20fa209b6e0bcae147dccf44e273b
Jean-Simon Desjardins <jsdesjardinssds@gmail.com>
2018-06-30 16:21:54 -0400

    Update bike_flasher.ino

--- bike_flasher.ino
+++ bike_flasher.ino
@@ -8,29 +8,22 @@
   const int CS = 11;
   const int CLK = 10; */
 
-const int leftButton = 7;
+/*const int leftButton = 7;
 const int rightButton = 6;
 const int rightLed = 5;
-const int leftLed = 4;
+const int leftLed = 4; */
 
-LedControl lc = LedControl (12,11,10,1);
-
-int leftButtonState = 0;
-int rightButtonState = 0;
+LedControl lc = LedControl (12,11,10,0);
 
 void setup() {
   lc.shutdown(0, false);
   lc.setIntensity(0, 10);
   lc.clearDisplay(0);
-
-  pinMode(rightButton, INPUT);
-  pinMode(leftButton, INPUT);
-  pinMode(rightLed, INPUT);
-  pinMode(leftLed, INPUT);
 }
 
 void rightArrow() {
-  byte right[8]  = {0x38,0x1c,0xe,0xff,0xff,0xe,0x1c,0x38};
+  //byte right[8]  = {0x38,0x1c,0xe,0xff,0xff,0xe,0x1c,0x38};
+  byte right [8] = {0x18,0x0c,0x06,0xff,0xff,0x06,0x0c,0x18};
   
   lc.setRow(0,0,right[0]);
   lc.setRow(0,1,right[1]);
@@ -46,7 +39,8 @@ void rightArrow() {
 }
 
 void leftArrow() {
-  byte left[8]  = {0x1c,0x38,0x70,0xff,0xff,0x70,0x38,0x1c};
+  //byte left[8]  = {0x1c,0x38,0x70,0xff,0xff,0x70,0x38,0x1c};
+  byte left[8] = {0x18,0x30,0x60,0xff,0xff,0x60,0x30,0x18};
   
   lc.setRow(0,0,left[0]);
   lc.setRow(0,1,left[1]);
@@ -62,14 +56,6 @@ void leftArrow() {
 }
 
 void loop() {
-  rightButtonState = digitalRead(leftButton);
-  leftButtonState = digitalRead(rightButton);
-  
-  if (leftButtonState == HIGH){
-    leftArrow();
-  } else if (rightButtonState == HIGH){
-    rightArrow();
-  } else {
-    lc.clearDisplay(0);
-  }
+  leftArrow();
+  //rightArrow();
 }
